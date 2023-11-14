a3d0369a41fdaa0e6152d24e1b23140e82678468
BATENDER <vinicius.torres.san@gmail.com>
2023-11-14 14:11:40 -0300

    v5 PawShop - Completo talvez?

--- js/index.js
+++ js/index.js
@@ -8,14 +8,17 @@ function mudaTema() {
     let body = document.querySelector('body')
     let temaAtual = document.querySelector('#temaAtual')
     let imgLogo = document.querySelector('#imgLogo')
+    let imgBtn = document.querySelector('.imgBtn')
 
     if (temaBnt.checked) {
         body.setAttribute('data-bs-theme', 'light')
         imgLogo.setAttribute('src', '../imgs/logo/logoDark.svg')
+        imgBtn.setAttribute('src', '../imgs/logo/arrow-up-circle.svg')
         temaAtual.innerHTML = 'Claro'
     } else {
         body.setAttribute('data-bs-theme', 'dark')
         imgLogo.setAttribute('src', '../imgs/logo/logoWhite.svg')
+        imgBtn.setAttribute('src', '../imgs/logo/arrow-up-circle-white.svg')
         temaAtual.innerHTML = 'Escuro'
     }
 }
